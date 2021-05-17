c38b59d2c64b7a773c1ec8d3bf510ee24529e526
Thomas <karlthomas.gustafsson@gmail.com>
2021-05-17 21:55:03 +0200

    fixed styling and textcomponent

--- src/App.vue
+++ src/App.vue
@@ -29,16 +29,42 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  width: 80vw;
+  margin: 0 auto;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+ .nav-section {
+ background-color: aquamarine;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.logo {
+ height: 10em;
+ width: 10em;
+ margin-left: 1em;
+}
+.nav {
+margin-right: 5em;
+}
+.footer {
+ background-color: aquamarine;
+ height: 15em;
+ width: 100%;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+}
+.socialMedia, .info {
+padding: 3em;
+text-align: center;
+padding-top: 0;
+}
+.mediaImage {
+width: 5em;
+height: 5em;
+}
+h5, h6 {
+ font-size: 1.2em;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
-   
+   <text-section rubrik="Rubrik 1" image="computer.png" />
+   <text-section rubrik="Rubrik 2" image="eagle.png" />
+   <text-section rubrik="Rubrik 3" image="tree.png" />
   </div>
 </template>
 
 <script>
+import TextSection from '../components/TextSection.vue'
 // @ is an alias to /src
 
 
 export default {
   name: 'Home',
   components: {
+    TextSection
    
   }
 }
